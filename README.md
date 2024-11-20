780cf2bfc407cfaef855e6320431a476a022c7fd
francesco ferruzzi <fferruzzi02@gmail.com>
2024-11-20 16:18:26 +0100

    lavoro iniziale su datasets.py (dati di nutri)

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## file datasets
 
 Il file datasets raccoglie i dati e li manipola con Polars
+
+## sitografia
+
+Tutti i dataset sono stati scaricati dal sito https://www.kaggle.com
+
+### Citations
+
+Dataset con le informazioni nutrizionali dei vari cibi -> nome variabile = nutri
+Utsav Dey. (2024). Food Nutrition Dataset [Data set]. Kaggle. https://doi.org/10.34740/KAGGLE/DSV/8820139
--- datasets.py
+++ datasets.py
@@ -1,2 +1,22 @@
 import polars as pl
+#import kagglehub #? perché ??? Ha senso usare questo o è un problema se i dati vengono cambiati???
 
+"""
+path = kagglehub.dataset_download("utsavdey1410/food-nutrition-dataset")
+
+print("Path to dataset files:", path)"""
+
+#todo: aprire i 5 datasets e unirli insieme in uno unico 
+#leggo i 5 datasets 
+nutri1 = pl.read_csv("nutri/FOOD-DATA-GROUP1.csv")
+nutri2 = pl.read_csv("nutri/FOOD-DATA-GROUP2.csv")
+nutri3 = pl.read_csv("nutri/FOOD-DATA-GROUP3.csv")
+nutri4 = pl.read_csv("nutri/FOOD-DATA-GROUP4.csv")
+nutri5 = pl.read_csv("nutri/FOOD-DATA-GROUP5.csv")
+
+#li concateno in uno unico, così ho una sola tabella
+nutri = pl.concat([nutri1,nutri2,nutri3,nutri4,nutri5])
+
+#studiamo un po' il dataset 
+print(nutri.glimpse(return_as_string=True)) 
+print(nutri.describe())
